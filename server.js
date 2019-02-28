ca734548e97e5371da56e3269c9ed5c5e6c51b02
ranranqi <1137625794@qq.com>
2019-02-28 17:17:40 +0800

    判断路径，跳转相应页面，路径不对返回错误

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 //创建server
 let http = require('http')
+let fs = require('fs')
 
-let server = http.createServer((req,res)=>{
+let server = http.createServer((req, res) => {
     console.log(req.url)
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'text/html')
-    res.write(`
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>你好</h1>
-    <p>今天星期四</p>
-</body>
-</html>
+    if (req.url === '/index.html') {
+        res.statusCode = 201
+        res.setHeader('Content-Type', 'text/html')
+        res.write(`
+            <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>你好</h1>
+            <p>今天星期四</p>
+        </body>
+        </html>
     `)
+    } else {
+        res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+        res.write('路径错误！！！')
+    }
     res.end()
 })
 server.listen(8000)
